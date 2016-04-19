d1a59067e95e0e0e6011cb65718dce26ec4bb086
Björn Fyrvall <bjorn.fyrvall@gmail.com>
2016-04-19 17:05:58 +0200

    Design changes

---
+++ Application/Views/Partial/Breadcrumbs.php
@@ -0,0 +1,7 @@
+<ul class="breadcrumb">
+    <li><a href="#">BonaJson</a> <span class="divider"/></li>
+    <li><a href="#">Classes</a> <span class="divider"/></li>
+    <li><a href="#">JObject</a> <span class="divider"/></li>
+    <li><a href="#">Properties</a> <span class="divider"/></li>
+    <li class="active">Value</li>
+</ul>
