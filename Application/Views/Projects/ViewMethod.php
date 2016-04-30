aecfd5b6622924f567fde83fe81c860a8054e800
Bona <bjorn.fyrvall@gmail.com>
2016-04-30 23:36:19 +0200

    Added Viewmthod view.

---
+++ Application/Views/Projects/ViewMethod.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bona
+ * Date: 2016-04-30
+ * Time: 23:34
+ */
--- Application/Views/Projects/ViewProject.php
+++ Application/Views/Projects/ViewProject.php
@@ -1,94 +1,41 @@
-<h1 class="page-header"><?php echo $Project->ProjectName;?></h1>
+<h1 class="page-header"><?php echo $Method->MethodName;?></h1>
+<div class="row">
+    <div class="col-lg-6 col-md-12">
+        <dl>
+            <dt>Return:</dt>
+            <dd>
+                <?php if($Method->ReturnType->ExternalSource === ''):?>
+                    <a href="<?php echo '/Projects/Details/' . $Method->ProjectName . '/Classes/' . $Method->Type->ClassName;?>">
+                        <?php echo $Method->ReturnType->ClassName;?>
+                    </a>
+                <?php else:?>
+                    <a target="_blank" href="<?php echo $Method->ReturnType->ExternalSource;?>">
+                        <?php echo $Method->ReturnType->ClassName;?>
+                    </a>
+                <?php endif;?>
+            </dd>
+        </dl>
+    </div>
+</div>
 
 <div class="row">
     <div class="col-lg-8">
-        <?php echo $Project->Description;?>
-    </div>
-</div>
-
-<h2>Classes</h2>
-<div class="row">
-    <div class="col-lg-12 col-md-12">
-        <div class="table-responsive">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <?php if($this->IsLoggedIn()):?>
-                        <th class="col-lg-2">Name</th>
-                        <th class="col-lg-8">Description</th>
-                        <th class="col-lg-2">&nbsp;</th>
-                    <?php else:?>
-                        <th class="col-lg-2">Name</th>
-                        <th class="col-lg-10">Description</th>
-                    <?php endif;?>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach($PublicClasses as $projectClass):?>
-                    <tr>
-                        <?php if($this->IsLoggedIn()):?>
-                            <td><?php echo $this->Html->Link("/Projects/Details/$Project->ProjectName/Classes/$projectClass->ClassName", $projectClass->ClassName);?></td>
-                            <td><?php echo $projectClass->ShortDescription;?></td>
-                            <td>
-                                <a class="btn btn-sm btn-default btn-margin-right" href="<?php echo "/Classes/Description/$projectClass->Id";?>"><span class="glyphicon glyphicon-align-left"</a>
-                                <a class="btn btn-sm btn-default btn-margin-right" href="<?php echo "/Classes/Edit/$projectClass->Id";?>"><span class="glyphicon glyphicon-pencil"</a>
-                                <a class="btn btn-sm btn-default btn-margin-right" href="<?php echo "/Classes/DeleteConfirm/$projectClass->Id";?>"><span class="glyphicon glyphicon-trash"</a>
-                            </td>
-                        <?php else:?>
-                            <td><?php echo $this->Html->Link("/Projects/Details/$Project->ProjectName/Classes/$projectClass->ClassName", $projectClass->ClassName);?></td>
-                            <td><?php echo $projectClass->ShortDescription;?></td>
-                        <?php endif;?>
-                    </tr>
-                <?php endforeach;?>
-                </tbody>
-            </table>
-        </div>
+        <?php echo $Method->Description;?>
     </div>
 </div>
 
 <?php if($this->IsLoggedIn()):?>
-    <h2>External Classes</h2>
     <div class="row">
-        <div class="col-lg-12 col-md-12">
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th class="col-lg-2">Name</th>
-                        <th class="col-lg-8">Description</th>
-                        <th class="col-lg-2">&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach($ExternalClasses as $projectClass):?>
-                        <tr>
-                            <td><?php echo $this->Html->Link("/Projects/Details/$Project->ProjectName/Classes/$projectClass->ClassName", $projectClass->ClassName);?></td>
-                            <td><?php echo $projectClass->ShortDescription;?></td>
-                            <td>
-                                <a class="btn btn-sm btn-default btn-margin-right" href="<?php echo "/Classes/Description/$projectClass->Id";?>"><span class="glyphicon glyphicon-align-left"</a>
-                                <a class="btn btn-sm btn-default btn-margin-right" href="<?php echo "/Classes/Edit/$projectClass->Id";?>"><span class="glyphicon glyphicon-pencil"</a>
-                                <a class="btn btn-sm btn-default btn-margin-right" href="<?php echo "/Classes/DeleteConfirm/$projectClass->Id";?>"><span class="glyphicon glyphicon-trash"</a>
-                            </td>
-                        </tr>
-                    <?php endforeach;?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-<?php endif;?>
-
-<?php if($this->IsLoggedIn()):?>
-    <div class="row">
-        <div class="col-lg-8">
-            <a class="btn btn-md btn-default" href="<?php echo "/Classes/Create/$Project->Id";?>">Create class</a>
+        <div class="col-lg-12">
+            <a class="btn btn-md btn-default" href="<?php echo "/Methods/Description/$Method->Id";?>">Edit description</a>
         </div>
     </div>
 <?php endif;?>
 
 <?php echo $this->PartialView('Documents', array('Documents' => $Documents, 'Project' => $Project));?>
-<?php echo $this->PartialView('SeeAlsoLinks', array('SeeAlsoLinks' => $SeeAlsoLinks), 'Project', $Project);?>
+<?php echo $this->PartialView('SeeAlsoLinks', array('SeeAlsoLinks' => $SeeAlsoLinks, 'Project', $Project));?>
 
 <?php if($this->IsLoggedIn()):?>
     <?php echo $this->PartialView('SeeAlsoLinkDialog');?>
 <?php endif;?>
+
